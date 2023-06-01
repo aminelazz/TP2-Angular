5a2170946c7bbcb55014f629f45c739e79a3bc90
aminelazz <simo.20052011@gmail.com>
2023-06-01 18:33:27 +0100

    Finishing header component + @ngx-translate/core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Function for loading translations from the assets folder
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbDropdownModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en', // Set the default language
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  selectedOption: string;
 
+  constructor(private translateService: TranslateService) {
+    this.selectedOption = 'English'; // Set 'Option 1' as the default selected option
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
+
+  getIconPath(option: string): string {
+    switch (option) {
+      case 'English':
+        return '/assets/en.png'; // Path for Option 1 icon
+      case 'French':
+        return '/assets/fr.png'; // Path for Option 2 icon
+      default:
+        return '';
+    }
+  }
+
+  changeLanguage(lang: string) {
+    this.translateService.use(lang); // Change the current language
+  }
 }
